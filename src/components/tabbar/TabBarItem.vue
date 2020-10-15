03e3a80cfc08e26584bcbcba25b285e56ebdc460
ice-ui <1275816862@qq.com>
2020-10-15 10:25:46 +0800

    基本结构

---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="tab-bar-item">
+    <!--   <img src="../../assets/img/tabbar/home.svg" alt="" />
+    <div>首页</div> -->
+    <!-- 动态决定图片和文字 -->
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :class="{ active: isActive }"><slot name="item-text"></slot></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "TabBarItem",
+  data() {
+    return {
+      isActive: true,
+    };
+  },
+};
+</script>
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+}
+.active {
+  color: red;
+}
+</style>
